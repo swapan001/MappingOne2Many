5fdb7b33beaecc1168b79c297649ff0bc03c64ad
swapan001 <swan03228@gmail.com>
2025-12-16 12:55:36 +0530

    create a new method in manager for add emp

--- src/main/java/entity/onetomanyBi/Employee.java
+++ src/main/java/entity/onetomanyBi/Employee.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 @Getter
 @Setter
@@ -18,5 +19,6 @@ public class Employee {
     private int empId;
     private String empName;
     @ManyToOne
+    @JoinColumn
     private Manager manager;
 }
--- src/main/java/entity/onetomanyBi/Manager.java
+++ src/main/java/entity/onetomanyBi/Manager.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +17,12 @@ import java.util.List;
 @ToString(exclude = "employees")
 @EqualsAndHashCode
 @Entity
+
 public class Manager {
     @Id
     private int mgrId;
     private String mgrName;
-    @OneToMany
+    @OneToMany(mappedBy = "manager")
     private List<Employee> employees;
     public  void addEmployee(Employee... newEmployees) {
         if (employees == null) {
